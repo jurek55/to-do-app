81480fb36f291678efb34082860b484982440052
Jurek Kunicki <jerzy.kunicki@gmail.com>
2020-03-10 19:27:01 +0100

    Updates

---
+++ precache-manifest.a9a9b18f8410f482c5054323454a7665.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fb7d6645da5f398eeb5c64174c2b1f3",
+    "url": "/to-do-app/index.html"
+  },
+  {
+    "revision": "ac2aedd7d84508b1115d",
+    "url": "/to-do-app/static/css/main.f77bc520.chunk.css"
+  },
+  {
+    "revision": "5822b80ccf29615f5e4e",
+    "url": "/to-do-app/static/js/2.f9974666.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/to-do-app/static/js/2.f9974666.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac2aedd7d84508b1115d",
+    "url": "/to-do-app/static/js/main.f87e3f47.chunk.js"
+  },
+  {
+    "revision": "4e25710608f16e161a4a",
+    "url": "/to-do-app/static/js/runtime-main.e4e02018.js"
+  }
+]);
